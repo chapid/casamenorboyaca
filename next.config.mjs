7e957b158de89948a3690b42063f3a293f607367
Viviana Vargas <lady.viviana.vargas@gmail.com>
2024-09-06 14:43:18 -0500

    Troubleshooting for amplify failed build

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+  
+};
+
+/* webpack: (config) => {
     
     config.resolve.alias.canvas = false;
     
     return config;
-  },
-};
-
+  },*/
 export default nextConfig;
