6b0924852bd5f9ce408da46e102eb055d43eeaad
Viviana Vargas <lady.viviana.vargas@gmail.com>
2024-09-06 14:50:55 -0500

    Remove pdf package completedly

--- components/PdfViewer.tsx
+++ components/PdfViewer.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 import { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf'
+//import { Document, Page, pdfjs } from 'react-pdf'
 import 'react-pdf/dist/Page/TextLayer.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.mjs';
+/*pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.mjs';*/
 interface PDFViewerProps {
     url: string;
 }
@@ -19,15 +19,7 @@ export function PDFViewer({ url }: PDFViewerProps) {
     }
     return (
         <div  ref={setContainerRef}>
-            <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
-            {Array.from(new Array(numPages), (_el, index) => (
-              <Page
-                key={`page_${index + 1}`}
-                pageNumber={index + 1}
-                width={containerRef?.clientWidth}
-              />
-            ))}
-            </Document>
+            
             
         </div>
     )
